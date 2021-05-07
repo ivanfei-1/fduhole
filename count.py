aa13b8186e301c3b8eaa16e6188222cf14763226
蛤失败 <34417826+hasbai@users.noreply.github.com>
2021-05-07 14:13:26 +0800

    讨论帖计数从零开始

--- count.py
+++ count.py
@@ -5,11 +5,11 @@ django.setup()
 from hole.models import *
 
 for d in Discussion.objects.all():
-    d.count = d.post_set.count()
+    d.count = d.post_set.filter(disabled__exact=False).count() - 1
     d.save()
 
 for t in Tag.objects.all():
-    t.count = t.discussion_set.count()
+    t.count = t.discussion_set.filter(disabled__exact=False).count()
     t.save()
 
 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '更新计数完成')
