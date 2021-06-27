8862298e22c35c39d981a7fc5a0ec4e91060e620
shi <jsclndnz@outlook.com>
2021-06-27 21:13:44 +0800

    bug

--- hole/admin.py
+++ hole/admin.py
@@ -4,10 +4,11 @@ from .models import *
 from django.utils.html import format_html
 from django.urls import reverse
 
+
 def linkify(field_name):
     """
     Converts a foreign key value into clickable links.
-    
+
     If field_name is 'parent', link text will be str(obj.parent)
     Link will be admin url for the admin url for obj.parent.id:change
     """
@@ -24,25 +25,31 @@ def linkify(field_name):
     _linkify.short_description = field_name  # Sets column name
     return _linkify
 
+
 class PostInline(admin.TabularInline):
     model = Post
 
 # class UserProfileInline(admin.TabularInline):
 #     model = UserProfile
 
+
 @admin.register(Discussion)
 class DiscussionAdmin(admin.ModelAdmin):
     inlines = [PostInline]
     fields = ('is_folded', 'disabled', 'tag', 'date_updated', 'date_created')
     readonly_fields = ('date_updated', 'date_created',)
-    list_display = ('__str__', 'is_folded', 'disabled','date_updated', 'date_created')
+    list_display = ('__str__', 'is_folded', 'disabled',
+                    'date_updated', 'date_created')
 
-@admin.register(Post)    
+
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     search_fields = ['content']
-    fields = ('content', 'discussion', 'username', 'reply_to', 'disabled')
+    fields = ('content', 'discussion', 'username',
+              'reply_to', 'disabled', 'delete_reason')
     readonly_fields = ('date_created',)
-    list_display = ('__str__', linkify('discussion'), 'username', 'reply_to', 'date_created', 'disabled')
+    list_display = ('__str__', linkify('discussion'), 'username',
+                    'reply_to', 'date_created', 'disabled')
 
 # @admin.register(User)
 # class UserAdmin(admin.ModelAdmin):
